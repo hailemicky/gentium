485daa2a02a2ec2b747a6a907d69518b38a246a2
Hailemichael Seleshi <seleshi17@gmail.com>
2022-11-23 20:30:56 +0300

    Nov 23,2022 - Update

--- app/Controllers/Http/ApplicationsController.ts
+++ app/Controllers/Http/ApplicationsController.ts
@@ -1,5 +1,6 @@
  import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
  import Application from 'App/Models/Application';
+ import Upload from '@ioc:Adonis/Core/Application'
 
 
 export default class ApplicationsController {
@@ -9,25 +10,41 @@ export default class ApplicationsController {
     public async apply({request,response,view}:HttpContextContract){
     
         const data= request.all();
+
+        const cv = request.file('cv_url')
+
+        console.log(cv)
+
+                if (cv) {
+
+
+                    await cv.move(Upload.publicPath('cv/uploads')).catch(console.error);
+
+                    try{
     
-        try{
+                        await Application.create(data);
+                     
+                         }
+                     
+                         catch(error){
+                     
+                             console.log(error)
+                     
+                             response.redirect().back()
+                     
+                     
+                     
+                        }
+
+
+                        return view.render('success_apply')
+                    
+                }
+                    
+     
     
-       await Application.create(data);
     
-        }
-    
-        catch(error){
-    
-            console.log(error)
-    
-            response.redirect().back()
-    
-    
-    
-       }
-    
-    
-       return view.render('success_apply')
+     
     
      }
 
